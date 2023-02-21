ef4346b6f0ebefb0598c2fcc7df29b7163f85ad0
Oğuzhan Tezcan <oguzhan.tezcan@habermetre.com>
2023-02-21 11:18:39 +0300

    Springboot server completed.

---
+++ src/main/java/com/tezcan/vodafone/HelloController.java
@@ -0,0 +1,15 @@
+package com.tezcan.vodafone;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Hello World from kubernetes!";
+	}
+
+}
---
+++ src/test/java/com/tezcan/vodafone/VodafoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tezcan.vodafone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VodafoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
